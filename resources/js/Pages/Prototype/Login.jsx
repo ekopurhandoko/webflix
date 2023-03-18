42c4f35e3d6a1ea132c76ef6d0c87e746347ce9e
ekopurhandoko <80388970+ekopurhandoko@users.noreply.github.com>
2023-03-18 09:08:19 +0700

    feat: slice login

--- resources/js/Pages/Prototype/Login.jsx
+++ resources/js/Pages/Prototype/Login.jsx
@@ -4,51 +4,51 @@
 // import { Link, Head } from "@inertiajs/react";
 
 export default function Login() {
-    return     <div class="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
-    <div class="fixed top-[-50px] hidden lg:block">
+    return     <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+    <div className="fixed top-[-50px] hidden lg:block">
         <img src="/images/signup-image.png"
-            class="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt=""/>
+            className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt=""/>
     </div>
-    <div class="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+    <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
         <div>
-            <img src="a/images/moonton-white.svg" alt=""/>
-            <div class="my-[70px]">
-                <div class="font-semibold text-[26px] mb-3">
+            <img src="/images/moonton-white.svg" alt=""/>
+            <div className="my-[70px]">
+                <div className="font-semibold text-[26px] mb-3">
                     Welcome Back
                 </div>
-                <p class="text-base text-[#767676] leading-7">
+                <p className="text-base text-[#767676] leading-7">
                     Explore our new movies and get <br/>
                     the better insight for your life
                 </p>
             </div>
-            <form class="w-[370px]">
-                <div class="flex flex-col gap-6">
+            <form className="w-[370px]">
+                <div className="flex flex-col gap-6">
                     <div>
-                        <label class="text-base block mb-2">Email Address</label>
+                        <label className="text-base block mb-2">Email Address</label>
                         <input type="email" name="email"
-                            class="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
+                            className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
                             placeholder="Email Address" />
                     </div>
                     <div>
-                        <label class="text-base block mb-2">Password</label>
+                        <label className="text-base block mb-2">Password</label>
                         <input type="password" name="password"
-                            class="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
+                            className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
                             placeholder="Password" />
                     </div>
                 </div>
-                <div class="grid space-y-[14px] mt-[30px]">
-                    <a href="/" class="rounded-2xl bg-alerange py-[13px] text-center">
-                        <span class="text-base font-semibold">
+                <div className="grid space-y-[14px] mt-[30px]">
+                    <a href="/" className="rounded-2xl bg-alerange py-[13px] text-center">
+                        <span className="text-base font-semibold">
                             Start Watching
                         </span>
                     </a>
-                    <a href="sign_up.html" class="rounded-2xl border border-white py-[13px] text-center">
-                        <span class="text-base text-white">
+                    <a href="sign_up.html" className="rounded-2xl border border-white py-[13px] text-center">
+                        <span className="text-base text-white">
                             Create New Account
                         </span>
                     </a>
-                    {/* <!-- <button type="submit" class="rounded-2xl bg-alerange py-[13px] text-center">
-                        <span class="text-base font-semibold">
+                    {/* <!-- <button type="submit" className="rounded-2xl bg-alerange py-[13px] text-center">
+                        <span className="text-base font-semibold">
                             Start Watching
                         </span>
                     </button> --> */}
